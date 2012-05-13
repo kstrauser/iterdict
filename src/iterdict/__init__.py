a9a86e6d4f3e5391014703d2fe0813aa487c504a
Kirk Strauser <kirk@strauser.com>
2012-05-13 15:09:49 -0700

    The Great Refactoring
    Changed __contains__ to fill in missing keys as it searches, and defined
    __missing__ to call __contains__ before giving up. Consequently, removed
    many overridden methods to allow their built-in equivalents to pick up the
    slack.

--- src/iterdict/__init__.py
+++ src/iterdict/__init__.py
@@ -28,24 +28,26 @@
 """Define a IterDict class which lazily evaluates initialization objects passed
 into it"""
 
+import sys
+from functools import wraps
+
 __all__ = ['IterDict']
 
 
-def _clonedictmethod(func):
-    """Copy docstrings from dict methods to the iterdict method with the same
-    name"""
-    # Python 3 dicts don't have all the same attributes as Python 2's dicts.
-    # In this context, that's not a big deal; just ignore it.
-    try:
-        newdoc = getattr(dict, func.__name__).__doc__
-    except AttributeError:
-        return func
-    olddoc = func.__doc__
-    if olddoc:
-        func.__doc__ = newdoc + '\n\n' + olddoc
-    else:
-        func.__doc__ = newdoc
-    return func
+@wraps(wraps)
+def dictmethod(wrapped):
+    from functools import WRAPPER_ASSIGNMENTS
+    wr_ass = set(WRAPPER_ASSIGNMENTS)
+    wr_ass.remove('__module__')
+    return wraps(wrapped, wr_ass)
+
+
+def findkeybeforedictmethod(func):
+    @dictmethod(func)
+    def inner(self, *args, **kwargs):
+        args[0] in self
+        return func(self, *args, **kwargs)
+    return inner
 
 
 class IterDict(dict):
@@ -60,7 +62,16 @@ class IterDict(dict):
     it won't overwrite a key present in the dict, and it won't create a key if
     that key has already been deleted from the dict. Surprises are bad."""
 
-    @_clonedictmethod
+    if sys.version_info[0] == 2:
+        __iteritems = dict.iteritems
+        __iterkeys = dict.iterkeys
+        __itervalues = dict.itervalues
+    else:
+        __iteritems = dict.items
+        __iterkeys = dict.keys
+        __itervalues = dict.values
+
+    @dictmethod(dict.__init__)
     def __init__(self, *args, **kwargs):
         super(IterDict, self).__init__()
 
@@ -68,62 +79,28 @@ class IterDict(dict):
         # requested.
         self.__iterator = None
 
-        try:
-            self.__iterkeys = super(IterDict, self).iterkeys
-        except AttributeError:
-            self.__iterkeys = super(IterDict, self).keys
-        try:
-            self.__itervalues = super(IterDict, self).itervalues
-        except AttributeError:
-            self.__itervalues = super(IterDict, self).values
-        try:
-            self.__iteritems = super(IterDict, self).iteritems
-        except AttributeError:
-            self.__iteritems = super(IterDict, self).items
         self.update(*args, **kwargs)
 
-    def __processiter(self):
-        """yield all of the values in self.__iterator until it's exhausted,
-        then mark it as such"""
-        if self.__iterator is None:
-            raise StopIteration
-        for key, value in self.__iterator:
-                self[key] = value
-                yield key, value
-        self.__iterator = None
-
-    @_clonedictmethod
+    @dictmethod(dict.__contains__)
     def __contains__(self, searchkey):
         if super(IterDict, self).__contains__(searchkey):
             return True
-        for key, value in self.__processiter():  # pylint: disable=W0612
+        if self.__iterator is None:
+            return False
+        for key, value in self.__iterator:
+            if not super(IterDict, self).__contains__(key):
+                super(IterDict, self).__setitem__(key, value)
             if key == searchkey:
                 return True
         return False
 
-    @_clonedictmethod
-    def __delitem__(self, searchkey):
-        try:
-            super(IterDict, self).__delitem__(searchkey)
-        except KeyError:
-            for key, value in self.__processiter():  # pylint: disable=W0612
-                if key == searchkey:
-                    break
-            else:
-                raise KeyError(searchkey)
+    def __missing__(self, searchkey):
+        if searchkey in self:
+            return self[searchkey]
+        else:
+            raise KeyError(searchkey)
 
-    @_clonedictmethod
-    def __getitem__(self, searchkey):
-        try:
-            return super(IterDict, self).__getitem__(searchkey)
-        except KeyError:
-            pass
-        for key, value in self.__processiter():
-            if key == searchkey:
-                return value
-        raise KeyError(searchkey)
-
-    @_clonedictmethod
+    @dictmethod(dict.__len__)
     def __len__(self):
         """Note: __len__ is only defined for iterdicts whose iterators have
         already been consumed. Otherwise, there's no way to tell how long those
@@ -131,91 +108,61 @@ class IterDict(dict):
         where 'key' can only be a dict key one time, not 1,000 times."""
         if self.__iterator is None:
             return super(IterDict, self).__len__()
-        raise TypeError("objects of type 'iterdict' sometimes have no defined len()")
+        raise TypeError("objects of type 'IterDict' sometimes have no defined len()")
 
-    @_clonedictmethod
+    @dictmethod(dict.__repr__)
     def __repr__(self):
         if self.__iterator is None:
             return super(IterDict, self).__repr__()
         return 'IterDict<%s, fed by %s>' % (super(IterDict, self).__repr__(),
             repr(self.__iterator))
 
-    @_clonedictmethod
-    def __setitem__(self, key, value):
-        if not super(IterDict, self).__contains__(key):
-            super(IterDict, self).__setitem__(key, value)
-
     #### Standard dict methods
 
-    @_clonedictmethod
-    def get(self, searchkey, default=None):
-        try:
-            return self.__getitem__(searchkey)
-        except KeyError:
-            return default
+    get = findkeybeforedictmethod(dict.get)
 
-    has_key = __contains__
-
-    @_clonedictmethod
-    def iterkeys(self):
-        for key, value in self.iteritems():
-            yield key
-
-    __iter__ = iterkeys
-
-    @_clonedictmethod
+    @dictmethod(__iteritems)
     def items(self):
-        return list(self.iteritems())
-
-    @_clonedictmethod
-    def itervalues(self):
-        for key, value in self.iteritems():
-            yield value
-
-    @_clonedictmethod
-    def iteritems(self):
         for item in self.__iteritems():
             yield item
-        for key, value in self.__processiter():
-            if not self.__contains__(key):  # FIXME
-                yield (key, value)
-            else:
-                print 'Already yielded', key
+        if self.__iterator is None:
+            raise StopIteration
+        for key, value in self.__iterator:
+            if not super(IterDict, self).__contains__(key):
+                super(IterDict, self).__setitem__(key, value)
+                yield key, value
+        self.__iterator = None
 
-    @_clonedictmethod
+    @dictmethod(__iterkeys)
     def keys(self):
-        """Note: This attempts to cast a potentially infinite generator to a
-        list. That is not guaranteed to work in finite time and space."""
-        return list(self.iterkeys())
+        for key, value in self.items():
+            yield key
 
-    @_clonedictmethod
-    def pop(self, searchkey, default=None):
+    @dictmethod(__itervalues)
+    def values(self):
+        for key, value in self.items():
+            yield value
+
+    pop = findkeybeforedictmethod(dict.pop)
+
+    @dictmethod(dict.popitem)
+    def popitem(self):
         try:
-            value = self.__getitem__(searchkey)
-            del self[searchkey]
-            return value
+            return super(IterDict, self).popitem()
         except KeyError:
-            if default is not None:
-                return default
-            raise
+            if self.__iterator is None:
+                raise KeyError('popitem(): dictionary is empty')
+            for key, value in self.__iterator:
+                if not super(IterDict, self).__contains__(key):
+                    return key, value
+            self.__iterator = None
+            raise KeyError('popitem(): dictionary is empty')
 
-    @_clonedictmethod
-    def popitem(self, searchkey):
-        value = self.__getitem__(searchkey)
-        del self[searchkey]
-        return (searchkey, value)
+    setdefault = findkeybeforedictmethod(dict.setdefault)
 
-    @_clonedictmethod
-    def setdefault(self, searchkey, default):
-        try:
-            return self.__getitem__(searchkey)
-        except KeyError:
-            self[searchkey] = default
-            return default
-
-    @_clonedictmethod
+    @dictmethod(dict.update)
     def update(self, iterator=None, **kwargs):
-        if not self.__iterator is None:
+        if self.__iterator is not None:
             raise ValueError('The current iterator is not yet exhausted')
         super(IterDict, self).update(**kwargs)
         if iterator is None:
@@ -233,8 +180,9 @@ class IterDict(dict):
         else:
             raise ValueError('%s is not iterable' % repr(iterator))
 
-    @_clonedictmethod
-    def values(self):
-        """Note: This attempts to cast a potentially infinite generator to a
-        list. That is not guaranteed to work in finite time and space."""
-        return list(self.itervalues())
+    #### IterDict-specific
+
+    def flatten(self):
+        """Update the IterDict with all keys from the iterator"""
+        for key, value in self.items():
+            pass
